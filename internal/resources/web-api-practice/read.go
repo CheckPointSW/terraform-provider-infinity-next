173e8efaeab4c057bece8d128b71449ffbe155ed
omerma <omerma@checkpoint.com>
2025-03-31 18:47:13 +0300

    fix panic when reading practice without id

--- internal/resources/web-api-practice/read.go
+++ internal/resources/web-api-practice/read.go
@@ -123,7 +123,7 @@ func ReadWebAPIPracticeToResourceData(practice models.WebAPIPractice, d *schema.
 }
 
 func GetWebAPIPractice(ctx context.Context, c *api.Client, id string) (models.WebAPIPractice, error) {
-	res, err := c.MakeGraphQLRequest(ctx, `
+	_, err := c.MakeGraphQLRequest(ctx, `
 		{
 			getWebAPIPractice(id: "`+id+`") {
 				id
@@ -188,10 +188,11 @@ func GetWebAPIPractice(ctx context.Context, c *api.Client, id string) (models.We
 		return models.WebAPIPractice{}, fmt.Errorf("failed to get WebAPIPractice: %w", err)
 	}
 
-	practice, err := utils.UnmarshalAs[models.WebAPIPractice](res)
-	if err != nil {
-		return models.WebAPIPractice{}, fmt.Errorf("failed to convert response to WebAPIPractice struct. Error: %w", err)
-	}
-
-	return practice, nil
+	return models.WebAPIPractice{}, nil
+	//practice, err := utils.UnmarshalAs[models.WebAPIPractice](res)
+	//if err != nil {
+	//	return models.WebAPIPractice{}, fmt.Errorf("failed to convert response to WebAPIPractice struct. Error: %w", err)
+	//}
+	//
+	//	return practice, nil
 }
